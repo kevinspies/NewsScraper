9bcca711e318a0df17e776d652f31d49af96fbff
Kevin Spies <kevin.spies@gmail.com>
2019-08-09 23:05:42 -0400

    artile modela nd imports

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);

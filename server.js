365d682ce93816c6f6ad60733c3b80e329381525
Kevin Spies <kevin.spies@gmail.com>
2019-08-10 17:56:12 -0400

    adding model dir, creating db in mongo shell, adding express functionality to server file

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-
 var express = require("express");
-var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var logger = require("morgan");
+// Require all models
+var db = require("./models");
+var PORT = 3000;
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/myapp");
+
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);

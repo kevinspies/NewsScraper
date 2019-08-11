e103fb359591ac4213d83acdee28c17668d0c55e
Kevin Spies <kevin.spies@gmail.com>
2019-08-11 16:24:14 -0400

    more mvc structures, bootsrapping my layout inside some handlebars files, deciding my page layout in my head, i'm thinking cnn vs fox?

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var logger = require("morgan");
+var exphbs = require("express-handlebars");
 // Require all models
-var db = require("./models");
+var db = require("./models"); //i will have to add an index.js file in models that exports all my collections
 var PORT = 3000;
 
 var app = express();
@@ -14,8 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/myapp");
-
 var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+  process.env.MONGODB_URI || "mongodb://localhost/newsScraperDB";
 mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});

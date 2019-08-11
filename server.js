9acc08eb792feef5ec5c5db0df737afc2b464b94
Kevin Spies <kevin.spies@gmail.com>
2019-08-11 18:37:14 -0400

    bootstrap in, handlebars / page loaded in

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ var cheerio = require("cheerio");
 var logger = require("morgan");
 var exphbs = require("express-handlebars");
 // Require all models
-var db = require("./models"); //i will have to add an index.js file in models that exports all my collections
-var PORT = 3000;
+var db = require("./models");
+var router = express.Router();
+var PORT = process.env.PORT || 3000;
+
+var goku = {
+  power: 9005
+};
 
 var app = express();
 
@@ -15,9 +20,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/newsScraperDB";
 mongoose.connect(MONGODB_URI);
+
+app.get("/", function(req, res) {
+  console.log("homepage get request recieved!");
+  res.render("index", goku); //passing in dummy data i just want to render the homepage
+  //with my three buttons
+  //   res.send("hello world");
+});
 
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
